3b275508a53751a291c61dda55d07fb516a031d0
Alex Rapley <rapley3@gmail.com>
2017-01-02 21:54:20 +1100

    Cleaned up redundant JS and added fade in and fade out effects

--- app.js
+++ app.js
@@ -3,27 +3,21 @@ var app = {};
 app.init = function() {
     $("#addItem").on('click', app.addItem);
     $(document).on('click', '.delete', app.deleteItem);
-    $(document).on('click', '.done', app.finishItem);
 };
 
 app.addItem = function() {
+    var text = $("#addToList").val();
     if ($("#addToList").val() !== "") {
-        var text = $("#addToList").val();
-        $("#todoList").append('<li class="list-group-item">' + text + ' ' + '<button class="btn btn-danger delete pull-right">Delete</button></li>');
+        $("#todoList").append('<li class="list-group-item">' + text + ' ' +
+        '<button class="btn btn-danger delete pull-right">Delete</button></li>').hide().fadeIn();
         $("#addToList").val("");
     }
 };
 
 app.deleteItem = function() {
-    $(this).parent().remove();
-};
-
-app.finishItem = function() {
-    if ($(this).parent().css('text-decoration') === 'line-through') {
-        $(this).parent().css('text-decoration', 'none');
-    } else {
-        $(this).parent().css("text-decoration", "line-through");
-    }
+    $(this).parent().fadeOut(function() {
+      $(this).remove();
+    });
 };
 
 app.init();
